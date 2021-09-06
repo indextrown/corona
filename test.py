d20bd1354c9fbece54b470a35a6249f92ca76fca
indextrown <69367698+indextrown@users.noreply.github.com>
2021-09-06 22:40:18 +0900

    Update test.py

--- test.py
+++ test.py
@@ -4,19 +4,14 @@ import bs4
 from bs4 import BeautifulSoup
 
  
-
-response = requests.get("http://xn--19-q81ii1knc140d892b.kr/main/main.do")
-
+url = "https://www.jinju.go.kr/05190/05641.web"
+response = requests.get(url, verify = False)
 soup = BeautifulSoup(response.text, 'lxml')
-response1 = requests.get("https://www.jinju.go.kr/05190/05641.web")
-soup1 = BeautifulSoup(response1.text, 'lxml')
 
 
-현재시간 = soup.select("div.top_area > p.exp")[0].text.replace('\t', '').replace('\r', '').replace('\n', '')
-입원중 = soup.select("div.situation1_1 p")[1].text.replace('\n', '')
-완치 = soup.select("div.situation1_1 p")[2].text.replace('\n', '')
-사망자 = soup.select("div.situation1_1 p")[3].text.replace('\n', '')
-계 = soup.select("div.situation1_1 p")[4].text.replace('\n', '')
+
+
+
 
 
 진주날짜 = soup1.select("div.hg1 > p")[0].text
@@ -46,20 +41,15 @@ with open('data.js', "r", encoding="UTF-8-sig") as f:
         line = f.readline()
 #파일에 변수명을 추가하여 다시 쓴다.
 final_data = f"var data = {data};"
-final_data = f"var 현재시간 = '{현재시간}';\n\
-var 입원중 = '{입원중}';\n\
-var 완치 = '{완치}';\n\
-var 사망자 = '{사망자}';\n\
-var 계 = '{계}';\n\
-var 진주날짜 = '{진주날짜}';\n\
-var 진주계 = '{진주계}';\n\
-var 진주완치 = '{진주완치}';\n\
-var 진주입원중 = '{진주입원중}';\n\
-var 진주사망자 = '{진주사망자}';\n\
-var 진주검사중 = '{진주검사중}';\n\
-var 진주검사결과 = '{진주검사결과}';\n\
-var 진주자가격리자 = '{진주자가격리자}';\n\
-var 진주거리두기단계 = '{진주거리두기단계}';\n\
+final_data = f"var 진주날짜 = '{진주날짜}';\n\
+var 계 = '{진주계}';\n\
+var 완치 = '{진주완치}';\n\
+var 입원중 = '{진주입원중}';\n\
+var 사망자 = '{진주사망자}';\n\
+var 검사중 = '{진주검사중}';\n\
+var 검사결과 = '{진주검사결과}';\n\
+var 자가격리자 = '{진주자가격리자}';\n\
+var 거리두기단계 = '{진주거리두기단계}';\n\
 " + final_data
 with open('data.js', "w", encoding="UTF-8-sig") as f_write:
     f_write.write(final_data) 
